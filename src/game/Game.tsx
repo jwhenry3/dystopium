312406efe0b49d30fdeb36fb597e374b8bc3e75b
Justin Henry <jhenry@cytracom.com>
2021-01-25 11:47:59 -0600

    test commit

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -1,26 +1,28 @@
-import React, {FC, useRef}         from "react";
-import TitleScene                  from "./scenes/title/TitleScene";
-import UnknownScene                from "./scenes/unknown/UnknownScene";
-import ExampleScene                from "./scenes/example/ExampleScene";
-import {usePhaserLifecycle}        from "./stores/game/phaser.store";
+import React, { FC, useRef } from "react";
+import TitleScene from "./scenes/title/TitleScene";
+import UnknownScene from "./scenes/unknown/UnknownScene";
+import ExampleScene from "./scenes/example/ExampleScene";
+import { usePhaserLifecycle } from "./stores/game/phaser.store";
 import "./Game.scss";
-import {SceneState, useSceneData} from "./stores/game/scene.store";
-
+import { SceneState, useSceneData } from "./stores/game/scene.store";
 
 const scenes: { [key: string]: FC } = {
   title: TitleScene,
-  example: ExampleScene
+  example: ExampleScene,
 };
-const getCurrentScene = ({currentScene}: SceneState) => scenes[currentScene] || UnknownScene;
+const getCurrentScene = ({ currentScene }: SceneState) =>
+  scenes[currentScene] || UnknownScene;
 
 function Game() {
   const Scene: FC = useSceneData(getCurrentScene);
   const ref = useRef<HTMLCanvasElement | null>(null);
   const phaser = usePhaserLifecycle(ref);
-  return <div className="game">
-    <canvas ref={ref}/>
-    {phaser.game && <Scene/>}
-  </div>;
+  return (
+    <div className="game">
+      <canvas ref={ref} />
+      {phaser.game && <Scene />}
+    </div>
+  );
 }
 
 export default Game;
