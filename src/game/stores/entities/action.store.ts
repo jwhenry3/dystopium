f8c06f223001a1bd6789775432bf3ad2fdbc17c8
Justin <jwhenry123@gmail.com>
2021-01-25 16:01:45 -0600

    moved contracts location reference

--- src/game/stores/entities/action.store.ts
+++ src/game/stores/entities/action.store.ts
@@ -1,13 +1,13 @@
-import create, {State} from "zustand";
-import {Actions}       from "../../../server/contracts";
+import create, { State } from "zustand";
+import { Actions } from "../../../server/contracts";
 
 export interface ActionData extends State {
-  actions: { [id: string]: Actions[] }
-  update: (data: { [id: string]: Actions[] }) => void
+  actions: { [id: string]: Actions[] };
+  update: (data: { [id: string]: Actions[] }) => void;
 }
-
 
 export const useActions = create<ActionData>((set, get) => ({
   actions: {},
-  update: (data: { [id: string]: Actions[] }) => set({actions: {...get().actions, ...data}})
+  update: (data: { [id: string]: Actions[] }) =>
+    set({ actions: { ...get().actions, ...data } }),
 }));
